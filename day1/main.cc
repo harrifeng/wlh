b54aed89c821d1db667bb0d6f1eb538a56af6940
Haoran Feng <haoran.feng@alo7.com>
2017-08-04 13:17:06 +0800

    first workable input output

---
+++ day1/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    int n, k;
+    while (scanf("%d%d", &n,&k ) != EOF) {
+        cout << n << endl;
+        cout << k << endl;
+    }
+    return 0;
+}
