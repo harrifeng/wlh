e06b91047f5ce991febf80c279e1c5925948b673
Haoran Feng <haoran.feng@alo7.com>
2017-08-04 13:51:02 +0800

    first workable

--- day1/main.cc
+++ day1/main.cc
@@ -7,8 +7,23 @@ int main(int argc, char *argv[])
 {
     int n, k;
     while (scanf("%d%d", &n,&k ) != EOF) {
-        cout << n << endl;
-        cout << k << endl;
+        int arr[100000] = {0};
+        for (int i = 0; i < n; i++) {
+            int cur;
+            scanf("%d", &cur);
+            arr[cur]++;
+        }
+        int cnt = 0;
+        for (int i = 0; i < 100000; i++) {
+            if (arr[i] > 0) {
+                cnt += 1;
+            }
+        }
+        if (k <= (n - cnt)) {
+            cout << cnt << endl;
+        } else {
+            cout << n - k << endl;
+        }
     }
     return 0;
 }
